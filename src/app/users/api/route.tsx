0a6d1f7f6ad712fe3f6760ea66544d314d21f2be
abubakir88 <abubakirsaidxonov88@gmail.com>
2024-05-13 06:16:00 +0500

    some bugs fixed

--- src/app/users/api/route.tsx
+++ src/app/users/api/route.tsx
@@ -1,21 +1,23 @@
 import { NextRequest } from "next/server";
-import { comments } from "./comments";
+import { users } from "./data";
 
 export function GET() {
-  // return new Response("<h1>Comments</h1>", {
+  // return new Response("<h1>users</h1>", {
   //   headers: {
   //     "Content-Type": "text/html",
   //   },
   // });
 
-  // return Response.json({ message: "Comments" });
+  // return Response.json({ message: "users" });
 
-  return Response.json(comments);
+  return Response.json(users);
 }
+
+let newUser: any;
 
 export async function POST(request: NextRequest) {
   const { text } = await request.json();
-  const newComment = { text, id: comments.length + 1 };
-  comments.push(newComment);
+  const newComment = { text, id: users.length + 1 };
+  users.push(newUser);
   return Response.json(newComment, { status: 201 });
 }
